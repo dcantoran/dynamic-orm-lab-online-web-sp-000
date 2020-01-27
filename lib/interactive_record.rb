dbc1943ce12843a1f68d305b2777216de9d7bc31
David Cantoran <davidcantoran36@gmail.com>
2020-01-27 23:19:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -64,7 +64,7 @@ class InteractiveRecord
     attr_key = attr.keys.join
     attr_val = attr.values.join
     
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attr_key} = #{attr_val} LIMIT 1"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attr_key} = '#{attr_val}' LIMIT 1"
     row = DB[:conn].execute(sql)
   end 
   
