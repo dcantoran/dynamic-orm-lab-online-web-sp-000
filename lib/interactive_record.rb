3a97f01bf7a070b3b5cac864f9f392778a34add9
David Cantoran <davidcantoran36@gmail.com>
2020-01-27 22:22:35 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
     
     DB[:conn].results_as_hash = true 
     
-    sql = "pragma table_info #{table_name}"
+    sql = "pragma table_info('#{table_name}')"
     
     table_info = DB[:conn].execute(sql)
     col_names = []
