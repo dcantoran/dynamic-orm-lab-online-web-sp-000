f86c23a6f7f0a832f29486d0a60331d6d7fffe3a
David Cantoran <davidcantoran36@gmail.com>
2020-01-27 22:36:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,8 +37,8 @@ class InteractiveRecord
   def values_for_insert
     values = []
     
-    self.class.column_names.each do ||
-      
+    self.class.column_names.each do |col_name|
+      values << "send'#{col_name}='"
     end 
   end 
   
